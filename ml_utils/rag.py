533931f1615d4cb6165f641d5a121a986d8c3cf4
yashveer jain <yashveerjain321@gmail.com>
2024-08-10 23:34:22 -0400

    added rag feature

---
+++ ml_utils/rag.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+import chromadb
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import PyPDFLoader
+
+from doc_embedding import get_langchain_embeddings
+from data_handler import get_docs
+
+
+class RAG:
+    def __init__(self, db_filepath, collection_name) -> None:
+        
+        self.urls = []
+
+        self.vector_store = Chroma(
+            collection_name=collection_name,
+            embedding_function = get_langchain_embeddings(),
+            persist_directory = db_filepath
+        )
+        pass
+    
+    def add_url(self, url):
+        doc_splits = get_docs([url])
+        # check if the url already exists in the vector store
+        #  “ids”, “embeddings”, “metadatas”, “documents”.
+        # self.vector_store.get()
+
+        self.vector_store.add_documents(doc_splits)
+        self.urls.append(url)
+    
+    def search(self, query, top_k = 3):
+        return self.vector_store.max_marginal_relevance_search(query,k=top_k)
+
+    def get_context(self, query):
+        docs = self.search(query, top_k=1)
+        return docs[0].page_content
+    
+    
+
+    
+    
